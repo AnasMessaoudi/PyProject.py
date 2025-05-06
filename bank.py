94c42068585eebbb66f04f73347b717ec47c0a0b
AnasMessaoudi <anesme05@gmail.com>
2025-05-06 20:59:32 +0100

    gui added

--- bank.py
+++ bank.py
@@ -1,24 +1,30 @@
 import time
 import os
 import data_base
+import gui 
+from tkinter import messagebox
 def clear_screen():
     time.sleep(2)
     os.system("cls")
-class Sign_in:
+class Sign_in(gui.Login):
     def __init__(self):
-        self.id=int(input("enter your id (only numbers allowed):"))
+        self.display()
+        self.id=int(self.retun_id())
         result=data_base.check_existance(self.id)
         while not result:
-            print("id not found !!")
-            self.id=int(input("enter your id:"))
+            self.show_id_error()
+            self.display()
+            self.id=int(self.retun_id())
             result=data_base.check_existance(self.id)
-        self.pw=input("enter your password: ")
+        self.pw=self.return_pass()
         test=data_base.check_pw(self.id,self.pw)
         c=0
         while test==False and c<3:
-            print("the password that you typed is false !!")
-            print(f"you have {3-c} trials left !!")
-            self.pw=input("enter your password: ")
+            if self.pw=='':
+                self.show_pass_error()
+            else:
+                self.show_pass_error2()
+            self.display()
             test=data_base.check_pw(self.id,self.pw)
             c+=1
         if test==False:
@@ -30,7 +36,6 @@ class Sign_in:
         self.duration=data_base.get_duration(self.id)
         self.current_money=data_base.get_current_money(self.id)
         self.__bank=data_base.get_bank(self.id)
-        
     def set_bank(self,bank):
         self.__bank =bank
     def get_bank(self):
@@ -39,38 +44,6 @@ class Sign_in:
         self.__salary=salary
     def get_salary(self):
         return self.__salary
-class BankAcc:#n7eb na3ml menu kima hadha lel clients el gdom w hadha n5alih lel clients el jdod 
-    def __init__(self):
-        self.id=0
-        self.name=''
-        self.age=0
-        self.salary=0
-        self.__pw=''
-        self.__bank=''
-        self.loan=0
-        self.duration=0
-        self.current_money=1200
-        while self.name=='' or self.age<18 or self.salary==0 or self.__pw=='':
-            if self.name=='':
-                self.name=input("enter your name:")
-            elif self.age<18:
-                self.age=int(input("enter you age (MUST BE OVER 18!!):"))
-            elif self.salary==0:
-                self.salary=int(input("enter your salary"))
-            elif self.__pw=='':
-                self.__pw=input("Choose your account's password :")
-    def set_bank(self,bank):
-        self.__bank=bank
-    def get_bank(self):
-        return self.__bank
-    def set_pw(self,pw):
-        self.__pw=pw
-    def get_pw(self):
-        return self.__pw
-    def get_salary(self):
-        return self.salary
-    def get_current_money(self):
-        return self.current_money
 class Bank:
     no_of_clients=0
     def __init__(self,Bname):
@@ -85,52 +58,84 @@ class Bank:
         self.no_of_clients+=1
     def get_no_of_clients(self):
         return self.no_of_clients
-class Menu:
+class BankAcc(gui.Signup):
+    bs=["attijari","al zaytouna","al islami"]
+    def __init__(self):
+        self.id=0
+        self.name=''
+        self.age=0
+        self.salary=0
+        self.__pw=''
+        self.__bank=''
+        self.loan=0
+        self.duration=0
+        self.current_money=1200
+        while self.name=='' or self.age<18 or self.salary==0 or self.__pw=='' or self.get_bank()=='' or self.find(self.get_bank())==False:
+            self.display()
+            self.name=self.return_name()
+            self.age=int(self.return_age())
+            self.salary=int(self.return_salary())
+            self.set_pw(self.return_pass())
+            self.set_bank(self.return_bank())
+            if self.name=='':
+                self.show_name_error()
+            elif self.age<18:
+                self.show_age_error()
+            elif self.salary==0:
+                self.show_salary_error()
+            elif self.__pw=='':
+                self.show_pass_error()
+            elif self.find(self.get_bank())==False or self.get_bank()=='':
+                if self.get_bank()=='':
+                    self.show_bank_error()
+                else:
+                    self.show_bank_error2()
+    def find(self,string):
+        for bank in self.bs:
+            if bank==string:
+                return True
+        return False 
+    def set_bank(self,bank):
+        self.__bank=bank
+    def get_bank(self):
+        return self.__bank
+    def set_pw(self,pw):
+        self.__pw=pw
+    def get_pw(self):
+        return self.__pw
+    def get_salary(self):
+        return self.salary
+    def get_current_money(self):
+        return self.current_money
+
+class Menu(gui.Home):
     def display_menu(self):
-        display=f"""
-               Welcome   !
-               please enter your choice (1 or 2):
-               1. Enter personal informations
-               2. Create an account 
-                """
-        print(display)
-        self.choice = int(input("choose an option(1 or 2):"))
+        r=0
+        while r!=1 and r!=2:
+            self.menu()
+            r= int(self.return_choice())
+        self.choice = r
         return self.choice
     def display_loan_menu(self):
-        op="""
-        1. Check the loan amount 
-        2. Check the remaining payment periode
-        3. Quit this menu
-        """
-        print(op)
         o=10
         while o!=2 and o!=1 and o!=3:
-            o=int(input("choose an option(1 or 2 or 3):"))
+            self.loan_menu()
+            o=int(self.return_choice())
         return o
-class Options:
+class Options(gui.Choices):
     def display_options(self):
-        op="""
-        1. You are trying to choose another bank 
-        2. You are staying at your current bank  
-        """
-        print(op)
         o=10
         while o!=2 and o!=1:
-            o=int(input("choose an option(1 or 2):"))
+            self.display()
+            o=int(self.return_choice())
         return o
     def display_options2(self):#matsir kan ki yabda bch yda5l infos mch bch ya3ml new account
-        op="""
-        1. Take a Loan
-        2. Check the amount of money in your account 
-        3. Withdraw an amount of money
-        4.Quit this menu  
-        """
-        print(op)
         o=10
         while o!=2 and o!=1 and o!=3 and o!=4:
-            o=int(input("choose an option(1 or 2 or 3 or 4):"))
+            self.display2()
+            o=int(self.return_choice2())
         return o
-class system:
+class system(gui.NewBank,gui.Choices):
     def __init__(self):
         self.banks=[Bank("attijari"),Bank("al zaytouna"),Bank("al islami")]
         self.menu=Menu()
@@ -140,89 +145,83 @@ class system:
             if bank.Bname==string:
                 return bank   
     def run(self):
-        r=self.menu.display_menu()
+        r=int(self.menu.display_menu())
         op=0
         if r==1:
             self.client=Sign_in()
             op=self.options.display_options()
             if op==1:
                 if self.client.loan==0:
-                    self.option1()
+                    new_bank=''
+                    test=False
+                    while test==False:
+                        self.diplay()
+                        new_bank=self.return_bank()
+                        for bank in self.banks:
+                            if bank.Bname==new_bank:
+                                test=True
+                                break
+                        if test==False:
+                            messagebox.showerror("ERROR","The bank that you typed is not in our system pls enter an existing bank")
+                    self.client.set_bank(new_bank)
+                    r=self.find(self.client.get_bank())
+                    r.Add_client(self.client)        
                     self.client.current_money=self.client.get_salary()
                     data_base.update_client_bank(self.client.id,self.client.get_bank())
+                    messagebox.showinfo("DONE","YOU CHANGED THE BANK SUCCESSFULLY")
                 else:
-                    print("YOU CAN CHOOSE ANOTHER BANK ONLY AFTER YOU PAY YOUR LOAN !!!")
+                    messagebox.showinfo("WARNING","YOU CAN CHOOSE ANOTHER BANK ONLY AFTER YOU PAY YOUR LOAN !!!")
             else:
                 while True:
-                    clear_screen()
                     if self.client.loan!=0:
                         while True:
                             k=self.menu.display_loan_menu()
                             if k==1 :
-                                print(f"the remaining amount of loan is {self.client.loan} ")
-                                clear_screen()
+                                messagebox.showinfo("LOAN AMOUNT",f"the remaining amount of loan is {self.client.loan}")
                             elif k==2:
-                                print(f"The remaining payment periode is {self.client.duration} months ")
-                                clear_screen()
+                                messagebox.showinfo("REMAINING DURATION",f"The remaining payment periode is {self.client.duration} months ")
                             else:
                                 break
                     op2=self.options.display_options2()
                     if op2==2:
-                        print(self.client.current_money)
-                        clear_screen()
+                        messagebox.showinfo("CURRENT MONEY",f"YOUR CURRENT BALANCE IS {self.client.current_money}")
                     elif op2==3:
-                        Q=self.client.current_money#el Q hadhi dima 0 tetsala7 ki na3ml data base nejbed menha infos
-                        amount=int(input("enter the amount of money you'd like to get :"))
+                        Q=self.client.current_money
+                        self.showw_op3()
+                        amount=int(self.return_withdraw_amount())
                         if amount>Q:
-                            print(f"YOU DON'T HAVE ENOUGH MONEY !! YOUR ACCOUNT BALANCE IS {self.client.current_money}")
+                            messagebox.showerror("ERROR",f"YOU DON'T HAVE ENOUGH MONEY !! YOUR ACCOUNT BALANCE IS {self.client.current_money}")
                         else:
-                            print("withdraw done successfully !!!")
+                            messagebox.showinfo("THANK YOU","withdraw done successfully !!!")
                             self.client.current_money-=amount
                             data_base.update_client_current_money(self.client.id,self.client.current_money)
-                        clear_screen()
                     elif op2==1:
                         if self.client.loan != 0:
-                            print("YOU ALREADY HAVE A LOAN THAT YOU NEED TO PAY !!")
-                            clear_screen()
+                            messagebox.showerror("ERROR","YOU ALREADY HAVE A LOAN THAT YOU NEED TO PAY !!")
                         else:
-                            x=int(input("enter the amount of money you'd like to take as a loan :"))                           
+                            self.show_op1()
+                            x=int(self.return_withdraw_amount())                           
                             self.client.loan+=x
                             data_base.update_client_loan(self.client.id,x)
                             self.client.set_salary(self.client.get_salary()*0.7)
                             data_base.update_client_salary(self.client.id,self.client.get_salary())
                             self.client.duration=(x//(self.client.get_salary()*0.3))+1
                             data_base.update_client_duration(self.client.id,self.client.duration)
-                            print("you took the loan successfully !!")
-                            print("from now on we are taking 30% from your salary to pay your debt ")
-                            print(f"your current salary is {self.client.get_salary()}")
-                            print(f"this will proceed for {self.client.duration} months")
-                            clear_screen()
+                            info=f"""you took the loan successfully !!
+from now on we are taking 30% from your salary to pay your debt                
+        your current salary is {self.client.get_salary()}  
+                     this will proceed for {self.client.duration} months"""
+                            messagebox.showinfo("info",info)
                     else:
                         break
         else:
             self.client=BankAcc()
-            self.client.current_money=self.client.salary
-            self.option1()   
-            #nsobouh fl data base
+            self.client.current_money=self.client.salary 
             self.client.id=data_base.add_client_to_db(self.client.get_bank(),self.client.name,self.client.age,self.client.get_salary(),self.client.get_pw(),self.client.loan,self.client.duration,self.client.current_money)
-            print(f"Your id is {self.client.id} . YOU MUST REMEMBER THAT SO YOU CAN ACCESS YOUR ACCOUNT !!")
-    def  option1(self):
-        new_bank=''
-        test = False
-        while test==False:
-            new_bank=input("enter the bank you would like to choose: ")
-            for bank in self.banks:
-                if bank.Bname==new_bank:
-                    test=True
-                    break
-            if test==False:
-                print("The bank that you typed is not in our system pls enter an existing bank")
-        self.client.set_bank(new_bank)
-        r=self.find(self.client.get_bank())
-        r.Add_client(self.client) 
-
+            messagebox.showinfo("IMPORTANT",f"Your id is {self.client.id}.YOU MUST REMEMBER THAT SO YOU CAN ACCESS YOUR ACCOUNT !!")
 data_base.show_clients()
 s=system()
 s.run()
 data_base.show_clients()
+#nzid fazet el transfere etda5l montant w el id mta3 elli t7eb ta3mellah transfere
 #nzidou faza bl wa9t yabda kol chhar yna9es ml loan el flous elli y9osouhom 3lih w yna9s el duration bchhar w nzid fazet forgot password wla forgot id
--- gui.py
+++ gui.py
@@ -1,7 +1,218 @@
 from tkinter import *
-interface= Tk()
-interface.title("BANK APP")
-interface.geometry("500x300")
-button=Button(interface,text="enter",fg="#121010",bg="#03fcb6",font="Roboto")
-button.pack()
-interface.mainloop()
+from tkinter import messagebox
+class Home():
+    def menu(self):
+        self.window=Tk()
+        self.window.title("HOME")
+        self.window.geometry("340x440")
+        self.txt=Label(self.window,text="please enter your choice (1 or 2):")
+        self.op1=Label(self.window,text="1. Enter personal informations ")
+        self.op2=Label(self.window,text="2. Create an account")
+        self.txt.grid(row=0,column=3)
+        self.op1.grid(row=1,column=3)
+        self.op2.grid(row=2,column=3)
+        self.choice=Entry(self.window)
+        self.choice.grid(row=3,column=3)
+        submit_btn = Button(self.window, text="Submit", command=self.get_choice1)
+        submit_btn.grid(row=4, column=3, columnspan=2)
+        self.window.mainloop()
+    def get_choice1(self):
+        self.user_choice=self.choice.get()
+        self.window.destroy()
+    def return_choice(self):
+        return self.user_choice 
+    def loan_menu(self):
+        self.window=Tk()
+        self.window.title("LOAN MENU")
+        self.window.geometry("340x440")
+        self.op1=Label(self.window,text="1. Check the loan amount")
+        self.op2=Label(self.window,text="2. Check the remaining payment periode ")
+        self.op3=Label(self.window,text="3. Quit this menu")
+        self.op1.grid(row=0,column=3)
+        self.op2.grid(row=1,column=3)
+        self.op3.grid(row=2,column=3)
+        self.choice=Entry(self.window)
+        self.choice.grid(row=3,column=3)
+        submit_btn = Button(self.window, text="Submit", command=self.get_loan_menu_choice)
+        submit_btn.grid(row=4, column=3, columnspan=2)
+        self.window.mainloop()
+    def get_loan_menu_choice(self):
+        self.user_choice=self.choice.get()
+        self.window.destroy()
+    def return_choice(self):
+        return self.user_choice 
+class NewBank():
+    def diplay(self):
+        self.window=Tk()
+        self.window.title("NEW BANK")
+        self.window.geometry("340x440")
+        self.user_new_bank=Label(self.window,text="enter the name of the new bank")
+        self.user_new_bank.grid(row=1,column=1)
+        self.user_new_bank_entry=Entry(self.window)
+        self.user_new_bank_entry.grid(row=1,column=2)
+        self.btn=Button(self.window,text="SUBMIT",command=self.confirm)
+        self.btn.grid(row=3,column=2)
+        self.window.mainloop()
+    def confirm(self):
+        self.b=self.user_new_bank_entry.get()
+        self.window.destroy()
+    def return_bank(self):
+        return self.b
+class loan():
+    pass
+
+class Choices():
+    def display(self):
+        self.window=Tk()
+        self.window.title("BANK MENU")
+        self.window.geometry("340x440")
+        self.op1=Label(self.window,text="1. You are trying to choose another bank").grid(row=0,column=3)
+        self.op2=Label(self.window,text="2. You are staying at your current bank ").grid(row=1,column=3)
+        self.choice=Entry(self.window)
+        self.choice.grid(row=2,column=3)
+        submit_btn = Button(self.window, text="Submit", command=self.get_choice)
+        submit_btn.grid(row=4, column=3, columnspan=2)
+        self.window.mainloop()
+    def get_choice(self):
+        self.user_choice=self.choice.get()
+        self.window.destroy() 
+    def return_choice(self):
+        return self.user_choice   
+    def display2(self):
+        self.window=Tk()
+        self.window.title("OPTIONS")
+        self.window.geometry("340x440")
+        self.op1=Label(self.window,text="1. Take a Loan").grid(row=0,column=3)
+        self.op2=Label(self.window,text="2. Check the amount of money in your account  ").grid(row=1,column=3)
+        self.op3=Label(self.window,text="3. Withdraw an amount of money ").grid(row=2,column=3)
+        self.op4=Label(self.window,text="4.Quit this menu ").grid(row=3,column=3)
+        self.choice2=Entry(self.window)
+        self.choice2.grid(row=4,column=3)
+        submit_btn2 = Button(self.window, text="Submit", command=self.get_choice2)
+        submit_btn2.grid(row=5, column=3, columnspan=2)
+        self.window.mainloop()
+        #ennajm na3ml el traitement hni fi 3oudh fi el bank.py w na3ml affichage selon el option
+    def show_op1(self):
+        self.window=Tk()
+        self.window.title("OPTIONS")
+        self.window.geometry("340x440")
+        self.user_amount=Label(self.window,text="enter how much you would like to take as a loan").grid(row=1,column=1,columnspan=2)
+        self.user_amount_entry=Entry(self.window)
+        self.user_amount_entry.grid(row=1,column=3)
+        self.op1btn=Button(self.window,text="SUBMIT",command=self.get_op1)
+        self.op1btn.grid(row=3,column=2)
+        self.window.mainloop()
+    def get_op1(self):
+        self.amount=self.user_amount_entry.get()
+        self.window.destroy()
+    def return_loan_amount(self):
+        return self.amount
+    def showw_op3(self):
+        self.window=Tk()
+        self.window.title("OPTIONS")
+        self.window.geometry("340x440")
+        self.user_wamount=Label(self.window,text="enter how much you would like to withdraw").grid(row=1,column=1,columnspan=2)
+        self.user_wamount_entry=Entry(self.window)
+        self.user_wamount_entry.grid(row=1,column=3)
+        self.op3btn=Button(self.window,text="SUBMIT",command=self.get_op3)
+        self.op3btn.grid(row=3,column=2)
+        self.window.mainloop()
+    def get_op3(self):
+        self.wamount=self.user_wamount_entry.get()
+        self.window.destroy()
+    def return_withdraw_amount(self):
+        return self.wamount
+    def get_choice2(self):
+        self.user_choice2=self.choice2.get()
+        self.window.destroy()
+    def return_choice2(self):
+        return self.user_choice2
+class Login():
+    def display(self):
+        self.window=Tk()
+        self.window.title("log in")
+        self.window.geometry("340x440")
+        self.userid_label=Label(self.window,text="id")
+        self.userid_label.grid(row=1,column=2)
+        self.userpw_label=Label(self.window,text="password")
+        self.userpw_label.grid(row=2,column=2)
+        self.userpw_entry=Entry(self.window,show='*')
+        self.userpw_entry.grid(row=2,column=3)
+        self.userid_entry=Entry(self.window)
+        self.userid_entry.grid(row=1,column=3)
+        self.button=Button(self.window,text="log in",command=self.confirm)
+        self.button.grid(row=3,column=3)
+        self.window.mainloop()
+    def confirm(self):
+        self.user_id=self.userid_entry.get()
+        self.user_pass=self.userpw_entry.get()
+        self.window.destroy()
+    def retun_id(self):
+        return self.user_id
+    def return_pass(self):
+        return self.user_pass
+    def show_id_error(self):
+        messagebox.showerror("Error", "id not found")
+    def show_pass_error(self):
+        messagebox.showerror("Error", "non empty string required")
+    def show_pass_error2(self):
+        messagebox.showerror("Error", "wrong password")
+class Signup():
+    def display(self):
+        self.window=Tk()
+        self.window.title("SIGN UP")
+        self.window.geometry("340x440")
+        self.username=Label(self.window,text="enter your name:")
+        self.username.grid(row=1,column=2)
+        self.username_entry=Entry(self.window)
+        self.username_entry.grid(row=1,column=3)
+        self.userage_label=Label(self.window,text="enter age(MUST BE OVER 18!!):")
+        self.userage_label.grid(row=2,column=2)
+        self.userage_entry=Entry(self.window)
+        self.userage_entry.grid(row=2,column=3)
+        self.user_salary=Label(self.window,text="enter your salary:")
+        self.user_salary.grid(row=3,column=2)
+        self.user_salary_entry=Entry(self.window)
+        self.user_salary_entry.grid(row=3,column=3)
+        self.user_pass=Label(self.window,text="choose a password:")
+        self.user_pass.grid(row=4,column=2)
+        self.user_pass_entry=Entry(self.window)
+        self.user_pass_entry.grid(row=4,column=3)
+        self.user_bank=Label(self.window,text="choose a bank:")
+        self.user_bank.grid(row=5,column=2)
+        self.user_bank_entry=Entry(self.window)
+        self.user_bank_entry.grid(row=5,column=3)
+        self.button=Button(self.window,text="SIGN UP",command=self.confirm).grid(row=7,column=3)
+        self.window.mainloop()
+    def confirm(self):
+        self.name=self.username_entry.get()
+        self.salary=self.user_salary_entry.get()
+        self.bank=self.user_bank_entry.get()
+        self.age=self.userage_entry.get()
+        self.pw=self.user_pass_entry.get()
+        self.window.destroy()
+    def return_name(self):
+        return self.name
+    def return_age(self):
+        return self.age
+    def return_salary(self):
+        return self.salary
+    def return_pass(self):
+        return self.pw
+    def return_bank(self):
+        return self.bank
+    def show_name_error(self):
+        messagebox.showerror("Error", "YOU NEED TO ENTER YOUR NAME")
+    def show_age_error(self):
+        messagebox.showerror("Error", "YOU MUST BE OVER 18")
+    def show_salary_error(self):
+        messagebox.showerror("Error", "SALARY MUST BE OVER 0$")
+    def show_pass_error(self):
+        messagebox.showerror("Error", "non empty string required")
+    def show_bank_error(self):
+        messagebox.showerror("Error","YOU SHOULD ENTER A BANK NAME")
+    def show_bank_error2(self):
+        messagebox.showerror("Error","YOU SHOULD ENTER A VALID BANK NAME")
+        messagebox.showinfo("BANKS INFO","THESE ARE THA VALID BANKS :ATTIJARI, AL ZAYTOUNA, AL ISLAMI")
+l=Login()
+#el login na9sah el quit button el login ki nenzel 3laha lazmha t5arjni 
